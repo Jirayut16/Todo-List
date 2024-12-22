4e116a657de7987d663fa09c7bb5b7186d2e733f
Jirayut Sikkhacharoen <jirayut.who@gmail.com>
2024-12-22 19:34:10 +0700

    Add files via upload

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { SweetAlert } from "./SweetAlert";
+
+function AddTodo({ addTask }: any) {
+  const [todoValue, setTodoValue] = useState<string>("");
+
+  function getValue(e: React.ChangeEvent<HTMLInputElement>) {
+    setTodoValue(e.target.value);
+  }
+
+  function submitValue(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    // กำหนดว่าจะส่งอะไรไปบ้าง เก็บไว้ใน obj
+    const task = {
+      todoValue: todoValue,
+      date: new Date().toLocaleDateString(),
+    };
+    console.log(task);
+    // ส่ง task ไปยัง addTask addTask มี parameter
+    addTask(task);
+    setTodoValue(""); // reset value = ""
+    SweetAlert(task.todoValue);
+  }
+  return (
+    <div className="max-w-[700px] mt-16">
+      <h3 className="text-xl font-bold text-gray-400 flex md:hidden text-center mb-4">
+        Add new task
+      </h3>
+      <form
+        className="flex flex-row gap-4 justify-center items-center"
+        onSubmit={submitValue}
+      >
+        <label
+          htmlFor="new task"
+          className="text-lg font-semibold text-gray-400 md:flex hidden"
+        >
+          Add new task:
+        </label>
+
+        <input
+          type="text"
+          className="border-2 rounded-md px-2 md:px-4 py-2 text-lg"
+          placeholder="Add your task.."
+          onChange={getValue}
+          value={todoValue}
+          autoFocus
+          required
+        />
+
+        <button
+          type="submit"
+          className="bg-teal-500  dark:bg-[#FF6500] dark:hover:bg-[#FF6500]/80 px-4 py-2 rounded-md h-12 text-white text-lg md:text-xl font-semibold hover:bg-teal-600 duration-200 transition-all"
+        >
+          Add now
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddTodo;
